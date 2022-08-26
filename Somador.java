17af4d46299fa07801f0f9ed6111cb06d41dbfed
Diogo Patrick <72279781+Diogo-P@users.noreply.github.com>
2022-08-25 21:46:47 -0300

    Add files via upload

---
+++ Somador.java
@@ -0,0 +1,15 @@
+package Exercicio;
+import java.util.Scanner;
+
+public class Somador {
+	public static void main() {
+		int a,b,soma;
+		Scanner sc = new Scanner(System.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
+		
+		soma=a+b;
+		System.out.println(soma);
+	}
+
+}
